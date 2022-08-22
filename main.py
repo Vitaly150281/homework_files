8cf31a422a3524f55896452ed9e60b63963353e9
Vitaly Smirnov <vit200581@mail.ru>
2022-08-22 14:23:53 +0700

    change1 main.py

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from pprint import pprint
 
 file_name = 'cookbook.txt'
-
+cook_book = {}
 def read_cookbook(file_name):
     with open(file_name) as f:
-        cook_book = {}
         for line in f:
             dish_name = line.strip()
             cook_book[dish_name] = []
@@ -15,3 +14,16 @@ def read_cookbook(file_name):
                                              'measure': comp[2].strip()})
             f.readline()
     return cook_book
+
+def get_shop_list_by_dishes(dishes, person_count):
+    shop_list = {}
+    for dish in dishes:
+        if dish in cook_book:
+            for comp in cook_book[dish]:
+                if comp['ingredient_name'] in shop_list:
+                    shop_list[comp['ingredient_name']]['quantity'] += comp['quantity'] * person_count
+                else:
+                    shop_list[comp['ingredient_name']] = {'measure': comp['measure'],
+                                                          'quantity': (comp['quantity'] * person_count)}
+    return shop_list
+
